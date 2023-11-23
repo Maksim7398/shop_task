5ca0f0021965c406021c918119519b0964c4e9af
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2023-11-23 17:50:01 +0300

    first commit

---
+++ src/test/java/com/example/Shop_task1/ShopTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shop_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
