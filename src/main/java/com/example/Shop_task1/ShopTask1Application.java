23ce314888be4bfb0f369795aeff444eb910b398
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2023-11-23 19:23:25 +0300

    add some class

--- src/main/java/com/example/Shop_task1/ShopTask1Application.java
+++ src/main/java/com/example/Shop_task1/ShopTask1Application.java
@@ -8,6 +8,7 @@ public class ShopTask1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopTask1Application.class, args);
+		System.out.println("hello world");
 	}
 
 }
---
+++ src/main/java/com/example/Shop_task1/web/SomeClass.java
@@ -0,0 +1,4 @@
+package com.example.Shop_task1.web;
+
+public class SomeClass {
+}
