b5dc297e495fd3b20dd928cdcf67d9ba434f40f6
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2023-12-04 20:11:31 +0300

    corrected comments

--- src/main/java/com/example/shop/persist/repository/ProductRepository.java
+++ src/main/java/com/example/shop/persist/repository/ProductRepository.java
@@ -4,11 +4,8 @@ import com.example.shop.persist.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, UUID> , JpaSpecificationExecutor<ProductEntity> {
-    Optional<ProductEntity> findByArticle(String article);
-
+    boolean existsByArticle(String article);
 }
--- src/main/java/com/example/shop/service/ProductService.java
+++ src/main/java/com/example/shop/service/ProductService.java
@@ -1,13 +1,13 @@
 package com.example.shop.service;
 
+import com.example.shop.controller.request.CreateProductRequest;
 import com.example.shop.controller.request.UpdateProductRequest;
 import com.example.shop.exeption.ArticleAlreadyExistsException;
+import com.example.shop.exeption.ProductNotFoundExeption;
+import com.example.shop.mapper.ProductMapper;
 import com.example.shop.model.ProductDto;
 import com.example.shop.persist.entity.ProductEntity;
 import com.example.shop.persist.repository.ProductRepository;
-import com.example.shop.controller.request.CreateProductRequest;
-import com.example.shop.exeption.ProductNotFoundExeption;
-import com.example.shop.mapper.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -23,39 +24,48 @@ public class ProductService {
     private final ProductRepository repository;
 
     private final ProductMapper mapper;
+
     public final List<ProductDto> productList() {
         return mapper.listProduct(repository.findAll());
     }
+
     public final UUID save(final CreateProductRequest request) {
-        repository.findByArticle(request.getArticle()).ifPresent(product-> {
+        if (repository.existsByArticle(request.getArticle())) {
             throw new ArticleAlreadyExistsException("продукт с таким артикулом уже существует");
-        });
-            final ProductEntity productEntity = mapper.createProductRequest(request);
-            productEntity.setLastQuantityChange(LocalDateTime.now());
-            repository.save(productEntity);
-            log.info(productEntity + "saved");
-            return productEntity.getId();
+        }
+        final ProductEntity productEntity = mapper.createProductRequest(request);
+        productEntity.setLastQuantityChange(LocalDateTime.now());
+        repository.save(productEntity);
+        log.info(productEntity + "saved");
+
+        return productEntity.getId();
     }
+
     public final ProductDto getProductById(final UUID id) {
         return mapper.getProduct(repository.findById(id).orElseThrow(
                 () -> new ProductNotFoundExeption("there is no product with this ID")
         ));
     }
+
     public final void deleteProductById(final UUID id) {
         repository.findById(id).orElseThrow(
                 () -> new ProductNotFoundExeption("there is no product with this ID")
         );
         repository.deleteById(id);
     }
+
     public final ProductDto updateProduct(final UUID id, final UpdateProductRequest request) {
-        final ProductEntity productEntity =  repository.findById(id).orElseThrow(
+        final ProductEntity productEntity = repository.findById(id).orElseThrow(
                 () -> new ProductNotFoundExeption("there is no product with this ID")
         );
         log.info(productEntity.toString());
         if (!productEntity.getQuantity().equals(request.getQuantity())) {
             productEntity.setLastQuantityChange(LocalDateTime.now());
             log.info("quantity was be  changed");
+        } else if (repository.existsByArticle(request.getArticle()) && !request.getArticle().equals(productEntity.getArticle())) {
+            throw new ArticleAlreadyExistsException("продукт с таким артикулом уже существует");
         }
+        productEntity.setArticle(request.getArticle());
         productEntity.setTitle(request.getTitle());
         productEntity.setQuantity(request.getQuantity());
         productEntity.setCategory(request.getCategory());
