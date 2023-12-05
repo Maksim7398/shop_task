73d7a87b959c744bf715884e3e3b774c68988e51
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2023-12-05 12:35:32 +0300

    remove sheduler and GlobalException

--- src/main/java/com/example/shop/exeption/ProductNotFoundExeption.java
+++
@@ -1,7 +0,0 @@
-package com.example.shop.exeption;
-
-public class ProductNotFoundExeption extends RuntimeException {
-    public ProductNotFoundExeption(String message) {
-        super(message);
-    }
-}
