36f79fa71f7f40dce53b3d5bd75a9236ea02e351
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2023-12-14 15:22:34 +0300

    add check test method  exchange price

--- src/test/java/com/example/shop/service/ProductServiceImplTest.java
+++ src/test/java/com/example/shop/service/ProductServiceImplTest.java
@@ -140,5 +140,18 @@ class ProductServiceImplTest {
                 .isInstanceOf(ArticleAlreadyExistsException.class)
                 .hasMessageContaining("продукт с таким артикулом уже существует");
     }
+    @Test
+    public void  updatePriceForProduct_checkChangedPrice_test(){
+        stubEntity = ProductEntityBuilder.aProductEntity().build();
+        when(repositoryMock.findAll()).thenReturn(List.of(stubEntity));
+        final BigDecimal expected = stubEntity.getPrice().multiply(new BigDecimal(0.1)).add(stubEntity.getPrice());
+        underTest.updatePriceForProduct(0.1);
+
+        ArgumentCaptor<ProductEntity> captor = ArgumentCaptor.forClass(ProductEntity.class);
+        verify(repositoryMock).save(captor.capture());
+        ProductEntity actual = captor.getValue();
+
+        assertEquals(expected,actual.getPrice());
+    }
 
 }
