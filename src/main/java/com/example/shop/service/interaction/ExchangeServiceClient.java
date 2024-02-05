65ec7927323d4730b51ba5d5a5aa73282c8c8b63
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2024-02-05 16:02:37 +0300

    fix conflict

--- src/main/java/com/example/shop/service/interaction/ExchangeServiceClient.java
+++ src/main/java/com/example/shop/service/interaction/ExchangeServiceClient.java
@@ -1,6 +1,6 @@
 package com.example.shop.service.interaction;
 
-import com.example.shop.service.product.request_filter.ExchangeRateValue;
+import com.example.shop.currency.request_filter.ExchangeRateValue;
 
 public interface ExchangeServiceClient {
     ExchangeRateValue getExchangeRate();
--- src/main/java/com/example/shop/service/interaction/ExchangeServiceClientImpl.java
+++ src/main/java/com/example/shop/service/interaction/ExchangeServiceClientImpl.java
@@ -1,6 +1,6 @@
 package com.example.shop.service.interaction;
 
-import com.example.shop.service.product.request_filter.ExchangeRateValue;
+import com.example.shop.currency.request_filter.ExchangeRateValue;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheConfig;
@@ -17,17 +17,20 @@ import org.springframework.web.client.RestTemplate;
 public class ExchangeServiceClientImpl implements ExchangeServiceClient {
 
     private final RestTemplate restTemplate;
+
     @Override
     @Nullable
     @Cacheable(unless = "#result == null", value = "exchangeValue")
     public ExchangeRateValue getExchangeRate() {
         try {
-            String uri = "/exchangeValue";
-            ExchangeRateValue exchangeRateValue = restTemplate.getForObject(uri, ExchangeRateValue.class);
+            final String uri = "/exchangeValue";
+            final ExchangeRateValue exchangeRateValue = restTemplate.getForObject(uri, ExchangeRateValue.class);
             log.info("exchange rate value from service: " + exchangeRateValue.getExchangeRate().toString());
+
             return exchangeRateValue;
         } catch (Exception ex) {
             log.error(ex.getMessage());
+
             return null;
         }
     }
