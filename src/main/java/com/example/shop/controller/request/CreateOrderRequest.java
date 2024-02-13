21bf3442340ebcaea6eaebb130fe511c5655d49a
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2024-02-13 16:18:33 +0300

    complete task16/2

--- src/main/java/com/example/shop/controller/request/CreateOrderRequest.java
+++ src/main/java/com/example/shop/controller/request/CreateOrderRequest.java
@@ -1,12 +1,10 @@
 package com.example.shop.controller.request;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
-import java.util.UUID;
 
 @Data
 @Builder
